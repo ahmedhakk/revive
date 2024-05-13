d6abbe7aaf3bd065de1d6db2a8184d3b3687d15c
Ahmed Mohamed Ali <71728993+ahmedhakk@users.noreply.github.com>
2024-05-13 21:56:45 +0300

    update card styling

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -5,28 +5,29 @@ import Card from "../ui/Card";
 import OverallProgressChart from "../features/dashboard/OverallProgressChart";
 import CustomersChart from "../features/dashboard/CustomersChart";
 import RehabAtHomeChart from "../features/dashboard/RehabAtHomeChart";
+import classes from "./Dashboard.module.css";
 
 const Dashboard = () => {
   return (
     <>
       <h1>Dashboard</h1>
       <Row type="horizontal">
-        <Card>
+        <Card className={classes.flat}>
           <h3>Rehab duration</h3>
           <RehabDurationChart />
         </Card>
-        <Card>
+        <Card className={classes.flat}>
           <h3>Overall Progress</h3>
           <OverallProgressChart />
         </Card>
       </Row>
 
       <Row type="horizontal">
-        <Card>
+        <Card className={classes.flat}>
           <h3>Customers</h3>
           <CustomersChart />
         </Card>
-        <Card>
+        <Card className={classes.flat}>
           <h3>Rehab At Home</h3>
           <RehabAtHomeChart />
         </Card>
